df8c338b04449d56194c3107b612bfe06dcc1711
Shashangka Shekhar <shashangka@gmail.com>
2018-05-20 23:25:39 +0600

    reconf

--- SPACoreAngular.Web/bin/Debug/netcoreapp2.0/publish/wwwroot/app/main.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var platform_browser_dynamic_1=require("@angular/platform-browser-dynamic"),core_1=require("@angular/core"),module_1=require("./module"),platform=platform_browser_dynamic_1.platformBrowserDynamic();core_1.enableProdMode(),platform.bootstrapModule(module_1.AppModule);
