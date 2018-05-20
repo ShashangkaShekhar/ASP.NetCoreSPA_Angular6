8d79ab91b0b98440dc1550c965818aa3615a50f3
Shashangka Shekhar <shashangka@gmail.com>
2018-05-20 15:28:45 +0600

    ASP.Net Core-Angular6
    Single Page Application Using ASP.Net Core-Angular6

---
+++ SPACoreAngular.Web/clientapp/module.ts
@@ -0,0 +1,28 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+//Components
+import { AppComponent } from './component/app/component';
+import { HomeComponent } from './component/home/component';
+import { AboutComponent } from './component/about/component';
+import { UserComponent } from './component/user/component';
+
+//Routes
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'user', component: UserComponent }
+];
+
+@NgModule({
+    declarations: [AppComponent, HomeComponent, AboutComponent, UserComponent],
+    imports: [BrowserModule, HttpModule, FormsModule, ReactiveFormsModule, RouterModule.forRoot(routes)],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
